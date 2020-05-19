5884bce3c925966a10751180a1806997d7dba278
longer <937723369@qq.com>
2020-05-19 16:19:28 +0800

    :apple:feature:update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ hexo clean && hexo deploy
 # 然后上传代码到hexo-blog仓库
 git add --all 
 git commit  -m ':apple:feature:update'
-git push  origin master
+git push  origin master -f
 echo -e "\ngit commit successful!"
 
 # 使用hexo-deployer-git插件的时候就不需要Travis CI了
